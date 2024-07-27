f36f3ce8eacbc1a3ae7431221bc7cc4534df056f
Ava van Heerden 242011 <242011@virtualwindow.co.za>
2024-07-27 18:50:47 +0200

    Shopping cart

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,114 +1,152 @@
-function decreaseMars(){
-    if (document.getElementById('ticketsMars').value != 0){
-            if (document.getElementById('ticketsMars').value = parseInt(document.getElementById('ticketsMars').value) - 1);
-    } else{
-        document.getElementById('ticketsMars').value = 0;
+
+const allFilterItems = document.querySelectorAll('.filter-item');
+const allFilterBtns = document.querySelectorAll('.filter-btn');
+
+window.addEventListener('DOMContentLoaded', () => {
+    allFilterBtns[1].classList.add('active-btn');
+});
+
+allFilterBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        showFilteredContent(btn);
+    });
+});
+
+function showFilteredContent(btn){
+    allFilterItems.forEach((item) => {
+        if(item.classList.contains(btn.id)){
+            resetActiveBtn();
+            btn.classList.add('active-btn');
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+
+function resetActiveBtn(){
+    allFilterBtns.forEach((btn) => {
+        btn.classList.remove('active-btn');
+    });
+}
+
+
+if (document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded' , ready);
+}
+
+else{
+    ready();
+}
+
+
+ function ready(){
+    var removeCartItemButton = document.getElementsByClassName('btn-danger');
+    for (var i = 0 ; i < removeCartItemButton.length; i++){
+        var button = removeCartItemButton[i];
+        button.addEventListener('click', removeCartItem)
     }
-}
 
-function increaseMars(){
-    document.getElementById('ticketsMars').value = parseInt(document.getElementById('ticketsMars').value) + 1;
-}
-
-let totalMarsCost = 0;
-
-function totalMars(){
- totalMarsCost = parseInt(document.getElementById('ticketsMars').value) * 5000;
- document.getElementById('totalMars').innerHTML = "R " + totalMarsCost
-}
-
-function decreaseJupiter(){
-    if (document.getElementById('ticketsJupiter').value != 0){
-            if (document.getElementById('ticketsJupiter').value = parseInt(document.getElementById('ticketsJupiter').value) - 1);
-    } else{
-        document.getElementById('ticketsJupiter').value = 0;
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    for(var i = 0 ;i < quantityInputs.length ; i++){
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged);
     }
-}
-
-function increaseJupiter(){
-    document.getElementById('ticketsJupiter').value = parseInt(document.getElementById('ticketsJupiter').value) + 1;
-}
-
-let totalJupiterCost = 0;
-
-function totalJupiter(){
- totalJupiterCost = parseInt(document.getElementById('ticketsJupiter').value) * 5000;
- document.getElementById('totalJupiter').innerHTML = "R " + totalJupiterCost
-}
-
-function decreaseMoon(){
-    if (document.getElementById('ticketsMoon').value != 0){
-            if (document.getElementById('ticketsMoon').value = parseInt(document.getElementById('ticketsMoon').value) - 1);
-    } else{
-        document.getElementById('ticketsMoon').value = 0;
+    
+    var addToCartButtons = document.getElementsByClassName('shop-item-button');
+    for(var i = 0; i< addToCartButtons.length; i++){
+        var button = addToCartButtons[i];
+        button.addEventListener('click',addToCartClicked)
     }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+ }
+
+
+ function purchaseClicked(){
+     alert('Thank you for your purchase!!!');
+     var cartItems = document.getElementsByClassName('cart-items')[0];
+     while(cartItems.hasChildNodes()){
+         cartItems.removeChild(cartItems.firstChild)
+     }
+     updateCartTotal();
+ }
+
+function removeCartItem(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+    
 }
 
-function increaseMoon(){
-    document.getElementById('ticketsMoon').value = parseInt(document.getElementById('ticketsMoon').value) + 1;
-}
-
-let totalMoonCost = 0;
-
-function totalMoon(){
- totalMoonCost = parseInt(document.getElementById('ticketsMoon').value) * 5000;
- document.getElementById('totalMoon').innerHTML = "R " + totalMoonCost
-}
-
-function decreaseNeptune(){
-    if (document.getElementById('ticketsNeptune').value != 0){
-            if (document.getElementById('ticketsNeptune').value = parseInt(document.getElementById('ticketsNeptune').value) - 1);
-    } else{
-        document.getElementById('ticketsNeptune').value = 0;
+function  quantityChanged(event){
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0 ){
+        input.value = 1;
     }
+    updateCartTotal();
 }
 
-function increaseNeptune(){
-    document.getElementById('ticketsNeptune').value = parseInt(document.getElementById('ticketsNeptune').value) + 1;
+
+function addToCartClicked(event){
+    var button = event.target;
+    var shopItem = button.parentElement.parentElement;
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText;
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src;
+    addItemToCart(title,price,imageSrc);
+    updateCartTotal();
 }
 
-let totalNeptuneCost = 0;
+function addItemToCart(title, price, imageSrc){
+    var cartRow = document.createElement('tr');
+    cartRow.classList.add('cart-row');
+    var cartItems = document.getElementsByClassName('cart-items')[0];
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
 
-function totalNeptune(){
- totalNeptuneCost = parseInt(document.getElementById('ticketsNeptune').value) * 5000;
- document.getElementById('totalNeptune').innerHTML = "R " + totalNeptuneCost
-}
-
-function decreaseSaturn(){
-    if (document.getElementById('ticketsSaturn').value != 0){
-            if (document.getElementById('ticketsSaturn').value = parseInt(document.getElementById('ticketsSaturn').value) - 1);
-    } else{
-        document.getElementById('ticketsSaturn').value = 0;
+    for (i = 0; i< cartItemNames.length ; i++){
+        if(cartItemNames[i].innerText == title){
+            alert('This item already has added to the cart!');
+            return
+        }
     }
+    var cartRowContents = `
+
+        <td class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="50" height="50">
+            <span class="cart-item-title">${title}</span>                  
+        </td>
+        <td class="cart-item cart-column">
+            <span class="cart-price cart-column">${price}</span>
+        </td>
+        <td class="cart-item cart-column">
+            <input class="cart-quantity-input" type="number" value="1" style="width: 50px">
+            <button class="btn btn-danger" type="button">Remove</button>
+        </td>        
+    `;
+     
+            
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem);
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
-function increaseSaturn(){
-    document.getElementById('ticketsSaturn').value = parseInt(document.getElementById('ticketsSaturn').value) + 1;
-}
 
-let totalSaturnCost = 0;
-
-function totalSaturn(){
- totalSaturnCost = parseInt(document.getElementById('ticketsSaturn').value) * 5000;
- document.getElementById('totalSaturn').innerHTML = "R " + totalSaturnCost
-}
-
-function decreaseUranus(){
-    if (document.getElementById('ticketsUranus').value != 0){
-            if (document.getElementById('ticketsUranus').value = parseInt(document.getElementById('ticketsUranus').value) - 1);
-    } else{
-        document.getElementById('ticketsUranus').value = 0;
+function updateCartTotal(){
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    var total = 0;
+    for (var i = 0 ; i< cartRows.length ; i++){
+        var cartRow =cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        var price = parseFloat(priceElement.innerText.replace('Rs ' , ''))
+        var quantity = quantityElement.value;
+        total = total + (price * quantity);
+         
     }
+    total = Math.round(total * 100 )/100;
+    document.getElementsByClassName('cart-total-price')[0].innerText = 'Rs '+ total + '.00';
+ 
 }
-
-function increaseUranus(){
-    document.getElementById('ticketsUranus').value = parseInt(document.getElementById('ticketsUranus').value) + 1;
-}
-
-let totalUranusCost = 0;
-
-function totalUranus(){
- totalUranusCost = parseInt(document.getElementById('ticketsUranus').value) * 5000;
- document.getElementById('totalUranus').innerHTML = "R " + totalUranusCost
-}
-
